dc3f72d723e4502c19ccea7192404b86400bd08a
minh dong <minhdev@outlook.com>
2014-10-23 00:13:18 -0700

    Add simple signup
    Sample from book, learning

--- cartfolo/cartfolo/Controllers/HomeController.cs
+++ cartfolo/cartfolo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using cartfolo.Models;
 
 namespace cartfolo.Controllers
 {
@@ -11,8 +12,32 @@ namespace cartfolo.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Sample Index";
+            int hour = DateTime.Now.Hour;
+            string greeting = "Hi";
+            if (hour < 12)
+            {
+                greeting = "Good morning";
+            }
+            else
+            {
+                greeting = "Good Afternoon";
+            }
+            ViewBag.Greeting = greeting;
+            ViewBag.Username = User.Identity.Name;
             ViewBag.CurrentDate = DateTime.Now;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(GuestAccount account)
+        {
+            return View("Thank you!", account);
         }
 
         public ActionResult About()
